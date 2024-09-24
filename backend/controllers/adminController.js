bfc58aea1c31f200a5f6976c4abc3a0d479ee4ba
Tharanee De Silva <it21219702@my.sliit.lk>
2024-09-24 21:49:59 +0530

    NoSQL Injection

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,5 +1,8 @@
 const asyncHandler = require("express-async-handler");
 const Admin = require("../models/adminModel");
+const generateToken = require("../utils/generateToken");
+const sanitize = require('mongo-sanitize');
+const bcrypt = require("bcryptjs");
 
 // register user as a admin
 const registerAdmin = asyncHandler(async (req, res) => {
@@ -54,31 +57,77 @@ const salt = await bcrypt.genSalt(10);
 });
 
 // authenticate the admin
+
 const authAdmin = asyncHandler(async (req, res) => {
 	const { email, password } = req.body;
-
-	const admin = await Admin.findOne({ email });
-
-	if (!admin) {
+ 
+	// Sanitize the email input to prevent NoSQL injection
+	const sanitizedEmail = sanitize(email);
+ 
+	// Validate that email is a valid string (you can add more validation)
+	if (typeof sanitizedEmail !== "string" || !sanitizedEmail) {
 		res.status(400);
-		throw new Error("Invalid NIC or Password");
+		throw new Error("Invalid Email");
 	}
-
-	if (!(password === admin.password)) {
-		res.status(400);
-		throw new Error("Invalid Email or Password");
-	} else {
-		res.status(201).json({
-			_id: admin._id,
-			name: admin.name,
-			telephone: admin.telephone,
-			address: admin.address,
-			email: admin.email,
-			password: admin.password,
-			pic: admin.pic,
-		});
+ 
+	try {
+		const admin = await Admin.findOne({ email: sanitizedEmail }).exec();
+ 
+		if (!admin) {
+			res.status(400);
+			throw new Error("Invalid Email or Password");
+		}
+ 
+		// Compare the hashed password in the request body with the hashed password in the database
+		const isMatch = await bcrypt.compare(password, admin.password);
+ 
+		if (isMatch) {
+			res.status(201).json({
+				_id: admin._id,
+				name: admin.name,
+				telephone: admin.telephone,
+				address: admin.address,
+				email: admin.email,
+				password: admin.password,
+				pic: admin.pic,
+				token: generateToken(admin._id),
+			});
+		} else {
+			res.status(400);
+			throw new Error("Invalid Email or Password");
+		}
+	} catch (error) {
+		// Handle any other errors
+		console.error(error);
+		res.status(500).json({ message: "Internal Server Error" });
 	}
 });
+
+// const authAdmin = asyncHandler(async (req, res) => {
+// 	const { email, password } = req.body;
+
+// 	const admin = await Admin.findOne({ email });
+
+// 	if (!admin) {
+// 		res.status(400);
+// 		throw new Error("Invalid NIC or Password");
+// 	}
+
+// 	if (!(password === admin.password)) {
+// 		res.status(400);
+// 		throw new Error("Invalid Email or Password");
+// 	} else {
+// 		res.status(201).json({
+// 			_id: admin._id,
+// 			name: admin.name,
+// 			telephone: admin.telephone,
+// 			address: admin.address,
+// 			email: admin.email,
+// 			password: admin.password,
+// 			pic: admin.pic,
+// 		});
+// 	}
+// });
 
 // view admin profile
 const getAdminProfile = asyncHandler(async (req, res) => {
@@ -103,7 +152,10 @@ const updateAdminProfile = asyncHandler(async (req, res) => {
 		admin.email = req.body.email || admin.email;
 		admin.pic = req.body.pic || admin.pic;
 		admin.password = req.body.password || admin.password;
-		
+		if (req.body.password) {
+			const salt = await bcrypt.genSalt(10);
+			admin.password = await bcrypt.hash(req.body.password, salt);
+		}
 		const updatedAdmin = await admin.save();
 
 		res.json({
